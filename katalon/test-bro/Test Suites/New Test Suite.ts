11e204d5d733b4b1bd344e7474c2ae7da36ebc18
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2022-01-03 09:33:04 +0700

    test2

---
+++ katalon/test-bro/Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a9cd0040-92bd-4700-9111-3595c581afff</testSuiteGuid>
+   <testCaseLink>
+      <guid>fad10eef-02bb-4df6-9964-5dc4222f3372</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
